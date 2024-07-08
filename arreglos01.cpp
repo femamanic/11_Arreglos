7bf4a2000ee7da2fe262633011fcbf61051cba16
femamanic <femamanic@unjbg.edu.pe>
2024-07-07 20:20:42 -0500

    Corregir arreglos01.cpp : error en cálculo de porcentaje de ceros

--- arreglos01.cpp
+++ arreglos01.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main () {
     system("cls");
     char grad = 248;
-    int A[20], n, S = 0, C = 0;
+    int A[20], n, S = 0;
+    float C = 0, P;
     cout << "Ingrese la cantidad de elementos : "; cin >> n;
     for (int i = 0; i < n; i++) {
         cout << "Ingrese el dato n" << grad << i + 1 << " : "; cin >> A[i];
@@ -14,7 +15,7 @@ int main () {
         }
     }
     cout << C;
-    float P = (C / n) * 100;
+    P = (C / n) * 100;
     cout << "La suma de los elementos es : " << S << "\n";
     cout << "El porcentaje de ceros es : " << P << "%\n";
     return 0;
